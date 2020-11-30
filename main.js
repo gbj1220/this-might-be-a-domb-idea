ed4c46ef21d72d532ff534970aba8b85faf22575
Gregory.Johnson <3531op@gmail.com>
2020-11-30 15:05:23 -0500

    Completed Assignment

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-1
+const lorem =  document.querySelector('p')
+
+  lorem.style.color = 'lightblue'
+  lorem.textContent = 
+  `Lorem ipsum dolor amet viral meh selfies drinking vinegar, 
+  intelligentsia poke flannel twee paleo enamel pin cray.
+  Banjo celiac crucifix, kickstarter la croix air plant 
+  jianbing hashtag vinyl hell of man bun selvage schlitz 
+  banh mi. Tacos hella raclette quinoa blog, 
+  williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth 
+  gentrify quinoa enamel pin celiac master cleanse. 
+  Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami 
+  distillery austin hashtag. Food truck synth wayfarers, 
+  street art banh mi actually authentic. Bitters tousled 
+  tattooed vegan neutra pug hell of fixie chia unicorn 
+  letterpress.`
+
+const heading = document.querySelector('h1')
+  heading.style.fontSize = '5em'
+
+const item13 = document.querySelector('#item-13')
+  item13.style.opacity = '.5'
+
+const item3 = document.querySelector('#item-3')
+  item3.textContent = `> I say, Hi!`
+
+const image = document.querySelector('img')
+  image.src = (`http://www.tioxic.com/wp-content/uploads/trex_4.jpg`)
+  image.style.height = '300px'
+  
+const bananaMonkey = document.querySelector('#banana-monkey')
+  bananaMonkey.style.height = '300px'
+  
+const item16 = document.querySelector('#item-16')
+  item16.textContent = `Won't get fooled again.`
